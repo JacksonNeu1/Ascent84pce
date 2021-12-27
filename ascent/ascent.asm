669f0df9da569bc0ab79a87b287c8391e3a1131d
JacksonNeu1 <73857248+JacksonNeu1@users.noreply.github.com>
2021-12-27 11:39:05 -0500

    Add files via upload

--- ascent/ascent.asm
+++ ascent/ascent.asm
@@ -1,17 +1,23 @@
 
+#include "includes\ti84pce.inc"
+
  .assume ADL=1
  .org userMem-2
  .db tExtTok,tAsm84CeCmp
 
-draw_buffer:
-	.dl 0
+
+
 ;0E30000h + 0200h
 
 	call _homeup
 	call _ClrScrnFull
 
 	call _RunIndicOff		; turn off run indicator
-	di				; disable interrupts
+	di			
+	
+	
+	;jp time_test_start
+	
 	ld	hl,mpLcdPalette
 
 	ld	a,%00000000 ;GGGBBBBB
@@ -19,8 +25,36 @@ draw_buffer:
 	ld  a,%00000000 ; GRRRRRGG
 	inc hl
 	ld	(hl),a
-	inc hl
+	inc hl;1yellow
 	ld	a,%11100000 ;GGGBBBBB
+	ld	(hl),a
+	ld  a,%11111111 ; GRRRRRGG
+	inc hl
+	ld	(hl),a
+	
+	inc hl;2blue
+	ld	a,%00011111 ;GGGBBBBB
+	ld	(hl),a
+	ld  a,%00000000 ; GRRRRRGG
+	inc hl
+	ld	(hl),a
+	
+	inc hl;3r
+	ld	a,%00000000 ;GGGBBBBB
+	ld	(hl),a
+	ld  a,%01111100 ; GRRRRRGG
+	inc hl
+	ld	(hl),a
+	
+	inc hl;4g
+	ld	a,%11100000 ;GGGBBBBB
+	ld	(hl),a
+	ld  a,%10000011 ; GRRRRRGG
+	inc hl
+	ld	(hl),a
+	
+	inc hl;5 white
+	ld	a,%11111111 ;GGGBBBBB
 	ld	(hl),a
 	ld  a,%11111111 ; GRRRRRGG
 	inc hl
@@ -28,14 +62,17 @@ draw_buffer:
 
 	call _os_ClearVRAMLines	; set all of vram to index 255 (white)
 	ld	a,lcdBpp4
-	ld	(mpLcdCtrl),a	
+	ld (mpLcdCtrl),a	
+	
 
+	ld hl,vRam + (160*5)
+	ld (draw_bg_vram_addr),hl
+	ld hl,$000080
+	call draw_bg_line
+	
 
-	ld hl,vRam
-	ld (draw_buffer),hl
-	ld bc,320*120
-	ld a,$11
-	call _MemSet
+	
+	
 	ei
 	call _GetKey
 	di
@@ -49,8 +86,28 @@ exit_prgm:
 	ei				; reset screen back to normal
 	ret				; return to os
 	
+
+printHL:;=================REMOVE
+	push hl
+	call _os_ClearVRAMLines	; set all of vram to index 255 (white)
+	ld	a,lcdBpp16
+	ld (mpLcdCtrl),a
+	pop hl
+	call _DispHL
+	ei
+	call _GetKey
+	di
+	jp exit_prgm
+
+
+test_addr:
+	.dl 0
+
+draw_buffer:
+	.dl 0
+
 	
-#include "includes\ti84pce.inc"
+#include "timeTesting.txt"
 #include "drawSprite.txt"
 #include "spriteData.txt"
 #include "levelData.txt"
